844841630778c31aba064abf03aecf04b6c21e9c
githubNee <183540581@qq.com>
2017-04-23 23:21:15 +0800

    user sign up

--- model/user_model.js
+++ model/user_model.js
@@ -1,16 +1,26 @@
 var db = require('../util/db');
 
 function create(body) {
-	// var 
-	// 	password = body['password'],
+	var email = body['email'],
+		password = body['password'],
+		username = body['username'],
+		gender = body['gender'],
+		description = body['description'];
 
+	if (email && password && username && gender && description == undefined) 
+		return null;
+	if (email == '' || password == '')
+		return null;
 
-	// var user = {
-	// 	username: username,
-	// 	password: password
-	// }
+	var user = {
+		email: email,		
+		password: password,
+		username: username,
+		gender: gender,
+		description: description
+	}
 
-	// return user;
+	return user;
 }
 
 function select(sql, callback) {
@@ -20,16 +30,32 @@ function select(sql, callback) {
 }
 
 function check(email, callback) {
-	sql = "select count(*) as number from user where email=\'" + email + '\'';
+	var sql = "select count(*) as number from user where email=\'" + email + '\'';
 	db.do_query(sql, function(result) {
 		callback(result[0]['number']);
 	});
 }
 
+function insert(user, callback) {
+	var sql_user = "insert into user values (null,\'" + user.email + "\',\'" + user.password + "\',\'" + user.token + '\');';
+	db.do_query(sql_user, function() {
+		console.log('user created');
+
+		var sql_user_info = "insert into user_info values (\'" + user.token + '\',' + user.gender + ",\'" + user.username + "\',\'" + user.description + "\');";
+		db.do_query(sql_user_info, function () {
+			console.log('user info created');
+			callback('success');
+		});
+	});
+
+	
+}
+
 var user_model = {
 	create: create,
 	do_query: select,
-	check: check
+	check: check,
+	insert: insert
 }
 
 module.exports = user_model;
--- routes/account.js
+++ routes/account.js
@@ -4,18 +4,36 @@ var request = require('request');
 var underscore = require('underscore');
 var user_model = require('../model/user_model');
 
-router.router('/')
+router.route('/')
 // 用户注册
 .post(function(req, res, next) {
 	var user = user_model.create(req.body);
+	if (user == null)
+		res.sendStatus(400);
 
-	
+	var token = user.email + user.password;
+	user['token'] = token;
+
+	user_model.check(user.email, function(result) {
+		if(result == 1) {
+			res.sendStatus(401);
+		} else {
+			user_model.insert(user, function(result) {
+				res.sendStatus(201);
+			});
+		}
+	});
+
 });
 
 router.route('/exists')
 // 检测邮箱是否被注册
 .get(function(req, res, next) {
 	var email = req.query.email;
+
+	if (email == undefined) {
+		res.sendStatus(400);
+	}
 
 	user_model.check(email, function(result) {
 		if(result == 1) {
