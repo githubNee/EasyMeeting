7f109a43abeb01485c8a45b79eb6517b92634031
BlackZIjian <735809461@qq.com>
2017-04-23 12:24:32 +0800

    webRTC服务端完成

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var SkyRTC = require('skyrtc').listen(server);
 
 var path = require('path');
+var port = process.env.PORT || 3000;
+server.listen(port);
 var request = require('request');
 var bodyParser = require('body-parser');	
 
@@ -24,4 +28,34 @@ app.use((req,res,next)=>{
     res.send("404 not found");
 })
 
-app.listen(3000);
+SkyRTC.rtc.on('new_connect', function(socket) {
+    console.log('创建新连接');
+});
+
+SkyRTC.rtc.on('remove_peer', function(socketId) {
+    console.log(socketId + "用户离开");
+});
+
+SkyRTC.rtc.on('new_peer', function(socket, room) {
+    console.log("新用户" + socket.id + "加入房间" + room);
+});
+
+SkyRTC.rtc.on('socket_message', function(socket, msg) {
+    console.log("接收到来自" + socket.id + "的新消息：" + msg);
+});
+
+SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
+    console.log("接收到来自" + socket.id + "的ICE Candidate");
+});
+
+SkyRTC.rtc.on('offer', function(socket, offer) {
+    console.log("接收到来自" + socket.id + "的Offer");
+});
+
+SkyRTC.rtc.on('answer', function(socket, answer) {
+    console.log("接收到来自" + socket.id + "的Answer");
+});
+
+SkyRTC.rtc.on('error', function(error) {
+    console.log("发生错误：" + error.message);
+});
