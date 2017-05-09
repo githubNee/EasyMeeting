ae11e7d67a7fc34fb72876aa73beec960159e433
githubNee <183540581@qq.com>
2017-05-09 20:41:48 +0800

    ???

--- model/meeting_model.js
+++ model/meeting_model.js
@@ -15,8 +15,9 @@ function insert(teamId, meeting) {
 	if (meeting['outline'] == undefined)
 		meeting['outline'] = '';
 
-	var sql = 'insert into meeting values(null, \'' + meeting['name'] + '\',\' + meeting['start_time'] + '\',\'' + meeting['end_time'] + '\',-1,\'' + meeting['color'] + '\', \'' + meeting['introduction'] + '\',\'' + meeting['outline'] + '\',' + teamId + ');';
-	db.do_query(sql, function() {});
+	var sql = 'insert into meeting values(null, \'' + meeting['name'] + '\',\'' + meeting['start_time'] + '\',\'' + meeting['end_time'] + '\',-1,\'' + meeting['color'] + '\', \'' + meeting['introduction'] + '\',\'' + meeting['outline'] + '\',' + teamId + ');';
+	console.log(sql);
+	// db.do_query(sql, function() {});
 }
 
 function getMeetings(teamId, callback) {
