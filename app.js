87ac1e554ac9d1149f6dcbdf61a0670bd1da24e8
BlackZIjian <735809461@qq.com>
2017-05-11 15:23:24 +0800

    no message

--- app.js
+++ app.js
@@ -39,9 +39,6 @@ app.use('/api/team', team);
 var meeting = require('./routes/meeting');
 app.use('/api/team', meeting);
 
-
-
-
 options = {
         key: fs.readFileSync('./privatekey.pem'),
         cert: fs.readFileSync('./certificate.pem')
@@ -49,43 +46,43 @@ options = {
 var sserver = https.createServer(options, app);
 sserver.listen(3001, function () {
         console.log('Https server listening on port ' + 3001);
-});
-var SkyRTC = require('skyrtc').listen(sserver);
-
-SkyRTC.rtc.on('new_connect', function(socket) {
-    console.log('创建新连接');
-});
-
-SkyRTC.rtc.on('remove_peer', function(socketId) {
-    console.log(socketId + "用户离开");
-});
-
-SkyRTC.rtc.on('new_peer', function(socket, room) {
-    console.log("新用户" + socket.id + "加入房间" + room);
-});
-
-SkyRTC.rtc.on('socket_message', function(socket, msg) {
-    console.log("接收到来自" + socket.id + "的新消息：" + msg);
-});
-
-SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
-    console.log("接收到来自" + socket.id + "的ICE Candidate");
-});
-
-SkyRTC.rtc.on('offer', function(socket, offer) {
-    console.log("接收到来自" + socket.id + "的Offer");
-});
-
-SkyRTC.rtc.on('answer', function(socket, answer) {
-    console.log("接收到来自" + socket.id + "的Answer");
-});
-
-SkyRTC.rtc.on('error', function(error) {
-    console.log("发生错误：" + error.message);
-});
-app.get('/webrtc', function(req, res, next) {
-        res.sendFile('public/test.html', {root: __dirname});
-});
+// });
+// var SkyRTC = require('skyrtc').listen(sserver);
+//
+// SkyRTC.rtc.on('new_connect', function(socket) {
+//     console.log('创建新连接');
+// });
+//
+// SkyRTC.rtc.on('remove_peer', function(socketId) {
+//     console.log(socketId + "用户离开");
+// });
+//
+// SkyRTC.rtc.on('new_peer', function(socket, room) {
+//     console.log("新用户" + socket.id + "加入房间" + room);
+// });
+//
+// SkyRTC.rtc.on('socket_message', function(socket, msg) {
+//     console.log("接收到来自" + socket.id + "的新消息：" + msg);
+// });
+//
+// SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
+//     console.log("接收到来自" + socket.id + "的ICE Candidate");
+// });
+//
+// SkyRTC.rtc.on('offer', function(socket, offer) {
+//     console.log("接收到来自" + socket.id + "的Offer");
+// });
+//
+// SkyRTC.rtc.on('answer', function(socket, answer) {
+//     console.log("接收到来自" + socket.id + "的Answer");
+// });
+//
+// SkyRTC.rtc.on('error', function(error) {
+//     console.log("发生错误：" + error.message);
+// });
+// app.get('/webrtc', function(req, res, next) {
+//         res.sendFile('public/test.html', {root: __dirname});
+// });
 
 
 
